74ef518be7e2319119da024aed5d889a3593e6ae
madalinabalaita <madalinabalaita14@gmail.com>
2020-05-01 12:58:10 +0300

    Created a class LibraryDbContext which extends DbContext to help with the database implementation

---
+++ LibraryData/LibraryDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryDbContext:DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options) { }
+         public DbSet<Member> Members { get; set; }
+    }
+}
