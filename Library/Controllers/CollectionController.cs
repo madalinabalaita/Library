989efe263a7e91634610c13b117df44a556c9697
madalinabalaita <madalinabalaita14@gmail.com>
2020-05-01 18:25:02 +0300

    created controller and model for the Collectioon of books and movies

---
+++ Library/Controllers/CollectionController.cs
@@ -0,0 +1,41 @@
+﻿using Library.Models.Collection;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class CollectionController:Controller
+    {
+        private ILibraryItem _items;
+
+        public CollectionController(ILibraryItem items)
+        {
+            _items = items;
+        }
+        public IActionResult Index()
+        {
+            var itemModels = _items.GetAll();
+            var listingResult = itemModels
+                .Select(result => new ItemIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl=result.ImageUrl,
+                    AOD=_items.GetAOD(result.Id),
+                    DeweyNr=_items.GetDeweyNr(result.Id),
+                    Title=result.Title,
+                    Type=_items.GetType(result.Id)
+
+                });
+            var model = new ItemIndexModel()
+            { 
+                Items =listingResult
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ Library/Models/Collection/ItemIndexListingModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Collection
+{
+    public class ItemIndexListingModel
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        //author or director
+        public string AOD { get; set; }
+         public string Type { get; set; }
+
+        public string DeweyNr { get; set; }
+        public string NrCopies { get; set; }
+
+    }
+}
