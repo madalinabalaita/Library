adc2791a896d0c6f61062b865dd802c0245d7ff2
madalinabalaita <madalinabalaita14@gmail.com>
2020-05-02 09:07:20 +0300

    Created model and controller for the items in detail

--- Library/Controllers/CollectionController.cs
+++ Library/Controllers/CollectionController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Library.Controllers
 {
-    public class CollectionController:Controller
+    public class CollectionController : Controller
     {
         private ILibraryItem _items;
 
@@ -23,16 +23,39 @@ namespace Library.Controllers
                 .Select(result => new ItemIndexListingModel
                 {
                     Id = result.Id,
-                    ImageUrl=result.ImageUrl,
-                    AOD=_items.GetAOD(result.Id),
-                    DeweyNr=_items.GetDeweyNr(result.Id),
-                    Title=result.Title,
-                    Type=_items.GetType(result.Id)
+                    ImageUrl = result.ImageUrl,
+                    AOD = _items.GetAOD(result.Id),
+                    DeweyNr = _items.GetDeweyNr(result.Id),
+                    Title = result.Title,
+                    Type = _items.GetType(result.Id)
 
                 });
             var model = new ItemIndexModel()
-            { 
-                Items =listingResult
+            {
+                Items = listingResult
+            };
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var item = _items.GetById(id);
+            var model = new ItemDetailModel
+            {
+                ItemId = id,
+                Title = item.Title,
+                Year = item.Year,
+                Price = item.Price,
+                Status = item.Status.Name,
+                ImageUrl = item.ImageUrl,
+                AOD = _items.GetAOD(id),
+                CurrentLocation = _items.GetCurrentLocation(id).Name,
+                DeweyNr = _items.GetDeweyNr(id),
+                ISBN = _items.GetISBN(id),
+
+
+
+
             };
             return View(model);
         }
---
+++ Library/Models/Collection/ItemDetailModel.cs
@@ -0,0 +1,31 @@
+﻿using LibraryData.Models;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Collection
+{
+    public class ItemDetailModel
+    {
+        public int ItemId { get; set; }
+        public string Title { get; set; }
+        public string AOD { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+        public string ISBN { get; set; }
+        public string CurrentLocation { get; set; }
+        public string DeweyNr { get; set; }
+        public string Status { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string MemberName { get; set; }
+
+        public Borrow LattestBorrow { get; set; }
+
+        public IEnumerable<BorrowHistory> BorrowHistory { get; set; }
+        public IEnumerable<ItemHoldModel> CurrentHolds { get; set; }
+
+    }
+}
