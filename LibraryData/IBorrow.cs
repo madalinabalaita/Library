957a7c478519e61ebc089aac48a71953ee17d8e6
madalinabalaita <madalinabalaita14@gmail.com>
2020-05-02 09:58:15 +0300

    created a Borrow service that implement its interface IBorrow (class to be implemented)

---
+++ LibraryData/IBorrow.cs
@@ -0,0 +1,24 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+   public  interface IBorrow
+    {
+
+        IEnumerable<Borrow> GetAll();
+        Borrow GetById(int borrowId);
+        void Add(Borrow newBorrow);
+        void BorrowItem(int itemId, int librarySubscriptionId);
+        void ReturnItem(int itemid, int librarySubscriptionId);
+        void PlaceHold(int itemid, int librarySubscriptionId);
+        string GetCurrentHoldMemberName(int id);
+        DateTime GetCurrentHoldPlaced(int id);
+        IEnumerable<Hold> GetCurrentHolds(int id);
+        void MarkLost(int itemId);
+        void MarkFound(int itemId);
+
+    }
+}
---
+++ LibraryServices/BorrowService.cs
@@ -0,0 +1,66 @@
+﻿using LibraryData;
+using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryServices
+{
+    public class BorrowService : IBorrow
+    {
+        public void Add(Borrow newBorrow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BorrowItem(int itemId, int librarySubscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Borrow> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Borrow GetById(int borrowId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentHoldMemberName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCurrentHoldPlaced(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hold> GetCurrentHolds(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkFound(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkLost(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceHold(int itemid, int librarySubscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReturnItem(int itemid, int librarySubscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
