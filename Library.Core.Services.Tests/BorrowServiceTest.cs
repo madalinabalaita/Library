1509584680f030e5a77ec23a22ff0c6e0e8ec7e4
madalinabalaita <madalinabalaita14@gmail.com>
2020-05-19 20:48:21 +0300

    added test project for services test

---
+++ Library.Core.Services.Tests/BorrowServiceTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Library.Core.Services.Tests
+{
+    [TestClass]
+    public class BorrowServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
