8723cf44243753d4efa670aee8a362b9eec077d6
madalinabalaita <madalinabalaita14@gmail.com>
2020-05-19 18:54:37 +0300

    Added  a project for tests

---
+++ Library.Tests/Services/BorrowServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Library.Data;
+using Library.Data.Models;
+using Library.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+
+namespace Library.Tests.Services
+{
+   
+}
